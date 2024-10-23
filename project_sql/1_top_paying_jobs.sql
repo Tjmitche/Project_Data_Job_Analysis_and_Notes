1104469e9463ac9ee780521f4e3cbcf5058b5bf8
Tjmitche <tommymitchell912@gmail.com>
2024-10-23 10:56:50 -0400

    Work on question 2

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -3,27 +3,27 @@ Question: What are the top-paying data analyst jobs?
     - Identify the top 10 highest-paying Data Analyst roles that are available in the USA.
     - Focuses on job postings with specified salaries.
     - Why? Aims to highlight the top-paying opportunities for Data Analysts,
-         offering insights into employment options and location flexibility.
+         offering insights into employment options and location.
 */
 SELECT
-    job_postings_fact.job_id,
-    job_postings_fact.job_title,
-    company_dim.name AS company_name,
-    job_postings_fact.job_via,
-    job_postings_fact.salary_year_avg,
-    job_postings_fact.job_schedule_type,
-    job_postings_fact.job_work_from_home AS romote_working_status,
-    job_postings_fact.job_country,
-    job_postings_fact.job_location,
-    job_postings_fact.job_posted_date::Date
+    job_id,
+    job_title,
+    name AS company_name,
+    job_schedule_type,
+    salary_year_avg,
+    job_work_from_home AS romote_working_status,
+    job_country,
+    job_location,
+    job_posted_date::Date
 FROM
     job_postings_fact
 LEFT JOIN company_dim
     ON job_postings_fact.company_id = company_dim.company_id
 WHERE
-    job_postings_fact.job_title = 'Data Analyst'
-    AND job_postings_fact.job_country = 'United States'
-    AND job_postings_fact.salary_year_avg IS NOT NULL
+    job_title = 'Data Analyst'
+    AND job_country = 'United States'
+    AND salary_year_avg IS NOT NULL
 
 ORDER BY
-    job_postings_fact.salary_year_avg DESC
+    salary_year_avg DESC
+LIMIT 10;
