8f6b628b0f700892d3a9209e3b66f69114d3265c
Tjmitche <tommymitchell912@gmail.com>
2024-10-23 10:03:50 -0400

    Changes to top paying jobs

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -1,6 +1,6 @@
 /*
 Question: What are the top-paying data analyst jobs?
-    - Identify the top 10 highest-paying Data Analyst roles that are available remotely.
+    - Identify the top 10 highest-paying Data Analyst roles that are available in the USA.
     - Focuses on job postings with specified salaries.
     - Why? Aims to highlight the top-paying opportunities for Data Analysts,
          offering insights into employment options and location flexibility.
@@ -8,10 +8,11 @@ Question: What are the top-paying data analyst jobs?
 SELECT
     job_postings_fact.job_id,
     job_postings_fact.job_title,
-    company_dim.name,
+    company_dim.name AS company_name,
     job_postings_fact.job_via,
-    job_postings_fact.job_schedule_type,
     job_postings_fact.salary_year_avg,
+    job_postings_fact.job_schedule_type,
+    job_postings_fact.job_work_from_home AS romote_working_status,
     job_postings_fact.job_country,
     job_postings_fact.job_location,
     job_postings_fact.job_posted_date::Date
@@ -22,5 +23,7 @@ LEFT JOIN company_dim
 WHERE
     job_postings_fact.job_title = 'Data Analyst'
     AND job_postings_fact.job_country = 'United States'
+    AND job_postings_fact.salary_year_avg IS NOT NULL
+
 ORDER BY
-    job_postings_fact.salary_year_avg
+    job_postings_fact.salary_year_avg DESC
